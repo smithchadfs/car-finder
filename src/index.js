bbd98373686b37fd23077829218c07d51121974a
Chad Smith <clsmith5@student.fullsail.edu>
2025-11-26 18:23:33 -0600

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,2 +1,21 @@
 // Imports your SCSS stylesheet
 import './styles/index.scss';
+
+// Import car data
+import carData from './car-dataset.json';
+
+// App logic
+class CarFinder {
+    constructor(data) {
+        this.cars = data;
+    }
+
+    // Method to filter cars by year, make, model
+    findCars(year, make, model) {
+        return this.cars.filter(car => {
+            return car.year === year &&
+            car.make === make &&
+            car.model === model;
+        });
+    }
+}
